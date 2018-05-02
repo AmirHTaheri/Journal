d4b54989f379cb25ffa3dee561ee67395c096089
Amir Taheri <amir.houshang.taheri@gmail.com>
2018-05-02 11:24:17 +0200

    error handling

--- index.php
+++ index.php
@@ -8,9 +8,13 @@
   <body>
     <h1>something</h1>
     <?php
+    /* Showing Errors*/
     ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    /* Showing Errors*/
+
+
     $host = 'localhost';
     $db   = 'journal';
     $user = 'root';
