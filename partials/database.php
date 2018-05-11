e43ac450c18a2d87118492ac38948dc70add7422
Amir Taheri <amir.houshang.taheri@gmail.com>
2018-05-11 22:12:16 +0200

    DB address changed

--- partials/database.php
+++ partials/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $db = new PDO(
-  "mysql:host=localhost:8889;dbname=journal;charset=utf8", //source
-  "root",   //username
-  "root",   //password
+  "mysql:host=den1.mysql6.gear.host;dbname=journalx;charset=utf8", //source
+  "journalx",   //username
+  "#Vizzit#8772",   //password
   [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
